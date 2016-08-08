707427e01c5ab4becbc1d38341ea7eb909ebad70
Daniel Lim <hbt@markhamboards.com>
2016-08-07 22:20:33 -0400

    Optimize Lean NG2
    NG2 Optimized & Lean

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,9 +6,3 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent { }
-
-/*
-Copyright 2016 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
